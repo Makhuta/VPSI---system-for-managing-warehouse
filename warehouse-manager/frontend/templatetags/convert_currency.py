1b70f73aa9ae6458ba97d3a25fd8f8d9cab44034
Makhuta <matykubla@gmail.com>
2025-03-31 17:09:19 +0200

    Change
    - for each currency its correct symbol is used

--- warehouse-manager/frontend/templatetags/convert_currency.py
+++ warehouse-manager/frontend/templatetags/convert_currency.py
@@ -9,7 +9,8 @@ def convert_currency(value, arg):
     c = CurrencyConverter()
     from_c = 'EUR'
     to_c = arg.upper()
+    from currency_symbols import CurrencySymbols
     try:
-        return f'{c.convert(value, from_c, to_c):,.2f} {to_c}'.replace(",", " ")
+        return f'{c.convert(value, from_c, to_c):,.2f} {CurrencySymbols.get_symbol(to_c)}'.replace(",", " ")
     except:
-        return f'{value:,.2f} {from_c}'.replace(",", " ")
+        return f'{value:,.2f} {CurrencySymbols.get_symbol(to_c)}'.replace(",", " ")
