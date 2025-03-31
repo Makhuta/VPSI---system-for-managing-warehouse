1d01f28b28a1831ac1081e7af693cf61ef2fb874
Makhuta <matykubla@gmail.com>
2025-03-31 19:04:27 +0200

    Delete shedule_jobs.py

--- warehouse-manager/services/shedule_jobs.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from .services_functions import test
-scheduler = BackgroundScheduler()
-
-def schedule_jobs():
-    scheduler.add_job(test.test_1, 'cron', minute='*/1')
-    scheduler.add_job(test.test_2, 'cron', minute='*/1', second='*/30')
-    scheduler.start()
