95470a5649ed18922c020c798e690245bcada77b
Makhuta <matykubla@gmail.com>
2025-03-31 19:05:15 +0200

    Add
    - add print of threshold variable

--- warehouse-manager/services/services_functions/check_stock_level.py
+++ warehouse-manager/services/services_functions/check_stock_level.py
@@ -1,4 +1,5 @@
 def run(threshold=10, **kwargs):
+    print(f'Threshold = {threshold}')
     from database.models import Stocks
     for item in Stocks.objects.all():
         if item.quantity <= item.min_quantity:
